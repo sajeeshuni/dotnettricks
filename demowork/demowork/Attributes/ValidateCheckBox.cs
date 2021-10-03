02561ceb7bc4d0176eecc59d5758452b1152e142
sajeeshuni <53659888+sajeeshuni@users.noreply.github.com>
2021-10-03 21:31:59 +0530

    dotnet4pm

---
+++ demowork/demowork/Attributes/ValidateCheckBox.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace demowork.Attributes
+{
+    public class ValidateCheckBox: ValidationAttribute, IClientModelValidator
+    {
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            context.Attributes.Add("data-val-chkbox", ErrorMessage);
+        }
+        public override bool IsValid(object value)
+        {
+            if ((bool)value)
+                return true;
+            else return false;
+        }
+    }
+}
---
+++ demowork/demowork/Models/UserViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using demowork.Attributes;
+
+namespace demowork.Models
+{
+    public class UserViewModel
+    {
+        [DisplayName("Employee Name")]
+        [Required(ErrorMessage ="Employee name is required")]
+        public string Name  { get; set; }
+        
+        [DisplayName("User Name")]
+        [Required(ErrorMessage ="User name is required")]
+        public string Username { get; set; }
+        [DisplayName("Password")]
+        [Required(ErrorMessage ="Password required")]
+        public string Password { get; set; }
+
+        [DisplayName("Confirmation Password")]
+        [Required(ErrorMessage = "Confirmation password is required")]
+        [Compare("Password",ErrorMessage ="Password and Confirm password must match")]
+        public string  ConfirmPassword { get; set; }
+        [DisplayName("Contact Number")]
+        [RegularExpression("^[6,7,8,9]\\d{10}$",ErrorMessage ="Contact number is not correct")]
+        [Required(ErrorMessage ="Contact number required")]
+        public string Contact { get; set; }
+
+        
+        [DisplayName("City Name")]
+        [Required(ErrorMessage ="Select a city name")]
+        public int City { get; set; }
+        public string Gender { get; set; }
+        [ValidateCheckBox(ErrorMessage ="Please accept terms")]
+        public bool AcceptTerms { get; set; }
+
+    }
+}
